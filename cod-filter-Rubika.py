950ea102a274633571b3466b29e97304eaa99c7f
MOHAMMADCYBERIT <106967761+MOHAMMADCYBERIT@users.noreply.github.com>
2022-08-16 02:02:31 +0430

    Create cod-filter-Rubika.py

---
+++ cod-filter-Rubika.py
@@ -0,0 +1,248 @@
+from rubpy import Rubika
+from re import findall
+from time import sleep
+import asyncio
+
+import os
+import time
+import sys
+import datetime
+import requests
+
+
+
+red='\033[93m'  
+green='\033[32m' 
+blue='\033[36m' 
+pink='\033[35m'  
+rang='\033[34m' 
+yellow='/033[31m'  
+
+#dat
+dat = (datetime.datetime.today())
+yellow,green="\033[31m","\033[92m"
+
+
+
+print(f"{yellow} ")
+banner = (f"""  Filtering(taigr)""")
+for bnr in banner:
+        sys.stdout.write(bnr)
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+
+
+os.system("clear")
+print()
+
+
+
+print(f"{red} ")
+banner = (f"""
+
+taigr
+                        .X*#M@$!!  !X!M$$$$$$WWx:.
+                    :!!!!!!?H! :!$!$$$$$$$$$$8X:
+                    !!~  ~:~!! :~!$!#$$$$$$$$$$8X:
+                    :!~::!H!<   ~.U$X!?R$$$$$$$$MM!
+                    ~!~!!!!~~ .:XW$$$U!!?$$$$$$RMM!
+                    !:~~~ .:!M"T#$$$$WX??#MRMMDM!
+                {blue}      ~?WuxiW*   "#$$$$8!!!!??!!!
+                    :X- M$$$$       "T#$T~!8$WUXU~
+                    :%  ~#$$$m:        ~!~ ?$$$$$$
+                :!.-   ~T$$$$8xx.  .xWW- ~""##*"
+        .....   -~~:< !    ~?T#$$@@W@*?$$      /
+        W$@@M!!! .!~~ !!     .:XUW$W!~ "~:    :
+        #"~~.:x%!!  !H:   !WM$$$$Ti.: .!WUn+!
+        :::~:!!:X~ .: ?H.!u "$$$B$$$!W:U!T$$M~
+        .~~   :X@!.-~   ?@WTWo("*$$$W$TH$!
+        Wi.~!X$?!-~    : ?$$$B$Wu("**$RM!
+        $R@i.~~ !     :   ~$$$$$B$$en:`
+        ?MXT@Wx.~    :     ~"##*$$$$M~""")
+for bnr in banner:
+        sys.stdout.write(bnr)
+        sys.stdout.flush()
+        time.sleep(0.009)
+
+
+
+print("")
+print("")
+print("")
+print("")
+print(f"{rang} ")
+banner = (f"""{yellow}《{dat}
+ {rang}
+  ||||||||||||||||||||||||||||||||||||||||||||||||||
+        |||||||||                             ||||||||||||
+        ||||||||||       ||         ||        ||||||||||||
+        ||||||||||||||                     |||||||||||||||
+        ||||||||||||||||||||||||||||||||||||||||||||||||||
+        |||  |  |          {green} taigr{yellow}      |  |  |||
+        |||  |  |                                |  |  |||
+        ||||||||||||||||||||||||||||||||||||||||||||||||||
+        ||||||||||||||||||||||||||||||||||||||||||||||||||
+""")
+for bnr in banner:
+        sys.stdout.write(bnr)
+        sys.stdout.flush()
+        time.sleep(0.009)
+        
+print("")
+print("")
+print("")
+print("")
+print ("Filter")
+time.sleep(0.1)
+print ("Rubika")
+time.sleep(0.5)
+print ("bbr")
+time.sleep(0.1)
+print ("taigr")
+time.sleep(0.5)
+print("cod")
+time.sleep(0.1)
+
+print("")
+print("")
+get_infos: list = [] # get post info
+links = []
+
+up = input(""" ______________________________
+ |                            |
+ |                            |                             
+ |                            |
+ |                            |
+ |                            |
+ |code Account rubika (1)     |
+ |                            |
+ |code channel rubika (2)     |
+ |                            |
+ |code group rubika (3)       |
+ |                            |
+ |                            |
+ |                            |
+ |                            |
+ |                            |
+ |                            |
+ |                            |
+ |                            |
+ |____________________________|
+ 
+ 
+ ╼ ❯""")
+ 
+
+
+print (blue)
+print("\rcod..",end="",flush=False)
+time.sleep(1)
+print("\rcod....",end="",flush=False)
+time.sleep(1)
+print("\rcod.....",end="",flush=False)
+time.sleep(1)
+print("\rcod......",end="",flush=False)
+time.sleep(1)
+print("\rcod.......",end="",flush=False)
+time.sleep(0.5)
+print("\rcod........",end="",flush=False)
+time.sleep(0.5)
+print("\rcod.........",end="",flush=False)
+time.sleep(0.5)
+print("\rcod..........",end="",flush=False)
+time.sleep(1)
+print("\r cod          ",end="",flush=False)
+
+
+print("")
+print("")
+print("")
+
+
+
+os.system("clear")
+print()
+
+if up == "1":
+         print ("""  \033[41m(https://.filterRubika.dlit.http//((6.0.3.6/d///f//i///y.6.0.3.2.3.8.6.4.5.8.6.3.7.7.9.0.5.5.1.2.4.0.3.4)(6.0.3.6/d///f//i///y.6.0.3.2.3.8.6.4.5.8.6.3.7.7.9.0.5.5.1.2.4.0.3.4)\33[1;0m  """)
+
+
+
+
+if up == "2":
+         print ("""  \033[41m'(/5.4.4.5/f//a/y/8.8.6.9.6.7.9.5(X/KRuHR'5.4.4.5/f//a/y/8.8.6.9.6.7.9.5.5.8.6.5.9.9.6.2/hOaBCpiVffur0H((x01N)\x01Z\x0ec4vvnlhr3rpzg\xa9\x00r\x00\x00\x00\x00r\x00\x00\x00\x00\xdaHQouoCkdDX9Yv7+szXvLqA.5.8.6.5.9.9.6.2/)'\33[1;0m   """)
+
+
+
+
+if up == "3":
+
+        print("""" \033[41m(6.0.3.6/d///f//i//y.6.0.3.2.3.8.6.4.5.8.6.3.7.7.9.0.5.5.1.2.4.08.3.2.3/r//g/h4.9.8.1.2.9.3.6.8.5.7.7.7.4.1.5.3.5.5.6.3.4)(6.0.3.4.6/d///f//i///y.6.0.3'(/2.3.5.2/e////f.h/7.2.1.4.4.4.8.3.4.9.9.7.1.2.5.4/)'.2.3.8.6.4.5.8.6.3.7.7.9.0.5.5.1.2.4.0.3.4)'\33[1;0m""")
+async def main():
+	t: bool = False
+	while(t != True):
+		try:
+			message_info: str = await hack.getLinkFromAppUrl(post_link)
+			global get_infos
+			get_infos.append(message_info['message_id'])
+			get_infos.append(message_info['object_guid'])
+			t: bool = True
+		except:
+			t: bool = False
+
+	t: bool = False
+	while(t!=True):
+		try:
+			messages : list = await hack.getMessagesInterval(channel_guid, await hack.getChannelLastMessageId(channel_guid))
+			t:bool=True
+		except:
+			t:bool=False
+	for msg in messages:
+		try:
+			msg = msg['text']
+			group_link = findall(r"https://rubika.ir/joing/\w{32}", msg)
+			for link in group_link:
+				links.append(link)
+		except: pass
+	
+	while(1):
+		for link in links:
+			sleep(2)
+			t:bool=False
+			count:int=0
+			limit:int=5
+			while(count<5 and t!=True):
+				try:
+					group_guid:str= await hack.joinGroup(link)
+					group_guid: str = group_guid.get('data').get('group').get('group_guid')
+					t:bool=True
+					count += 5
+				except:
+					t:bool=False
+					count += 1
+			t:bool=False
+			count:int=0
+			limit:int=5
+			while(count<5 and t!=True):
+				try:
+				#	await bot.sendMessage(group_guid, "تبلیغ")
+					await hack.forwardMessages(get_infos[1], [get_infos[0]], group_guid)
+					print('hack*')
+					t:bool=True
+					count+=5
+				except:
+					t:bool=False
+					count+=1
+			t:bool=False
+			count:int=0
+			limit:int=5
+			while(count<5 and t!=True):
+				try:
+					await hack.leaveGroup(group_guid)
+					t:bool=True
+					count+=5
+				except:
+					t:bool=False
+					count+=1
+
